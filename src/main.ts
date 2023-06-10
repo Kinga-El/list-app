7a010427db9e9b688737480a60b65c8548e755b2
kinga.loose <kinga.loose@outlook.com>
2023-06-10 18:22:21 +0200

    eslint, prettier configuration, installing packages

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createPinia } from "pinia";
+
+export async function main() {
+  const pinia = createPinia();
+
+  const app = createApp(App).use(pinia);
 
 
+  app.mount(#app)
+}
 
 
